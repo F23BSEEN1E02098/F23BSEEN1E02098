0882b011d0c4b058273d8e763c7f0a3714542121
Minahil Naeem <minahiln46@gmail.com>
2023-11-20 10:55:49 -0800

    Example 4 done.

---
+++ example4.py
@@ -0,0 +1,13 @@
+# Example 4:Check if the first and last number of a list is the same
+number_x1 = [10, 20, 30, 40, 10]
+if number_x1[0] == number_x1[-1]:
+    print("For the first list, the first and last numbers are the same.")
+else:
+    print("For the first list, the first and last numbers are different.")
+
+# Second list
+number_x2 = [75, 65, 35, 75, 30]
+if number_x2[0] == number_x2[-1]:
+    print("For the second list, the first and last numbers are the same.")
+else:
+    print("For the second list, the first and last numbers are different.")
